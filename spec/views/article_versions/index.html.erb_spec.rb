5f77cea793601afe438c6800aa171307e2ca5769
如彼 <hua.qiuh@alibaba-inc.com>
2015-12-24 13:15:43 +0800

    add view specs

--- spec/views/article_versions/index.html.erb_spec.rb
+++ spec/views/article_versions/index.html.erb_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe "article_versions/index.html.erb", type: :view do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:article) { create :article }
+
+  it do
+    assign :article, article
+    assign :article_versions, article.versions
+    render
+
+    expect(rendered).to have_selector 'li'
+  end
 end
--- spec/views/article_versions/show.html.erb_spec.rb
+++ spec/views/article_versions/show.html.erb_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe "article_versions/show.html.erb", type: :view do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:article) { create :article }
+
+  it do
+    assign :article, article
+    assign :article_version, article.versions.last
+    render
+
+    expect(rendered).to have_selector '.article.version'
+  end
 end
